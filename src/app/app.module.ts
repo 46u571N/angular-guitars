b56cc64b95775b1eba419495dac24e9a161ab9b4
46u571N <leo.gardey@gmail.com>
2021-11-17 19:46:11 -0300

    Generar Componente about-us [Closes #5]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GuitarListComponent } from './guitar-list/guitar-list.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GuitarListComponent
+    GuitarListComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
