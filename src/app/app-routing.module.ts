b573e9332e67ee9eaa978f6da8b9b6a1bc2da9e7
46u571N <leo.gardey@gmail.com>
2021-11-17 21:34:50 -0300

    Configurar Routing [Closes #6]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { GuitarListComponent } from './guitar-list/guitar-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: '',redirectTo: 'guitars',pathMatch: 'full' },
+ { path: 'guitars', component: GuitarListComponent },
+ { path: 'about', component:  AboutUsComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
