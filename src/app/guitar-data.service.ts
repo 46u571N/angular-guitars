78d3bdfd4e7c183aa5698bd35be3c74a11f887b1
46u571N <leo.gardey@gmail.com>
2021-11-25 16:03:56 -0300

    Modificar URL ( enviroments ; enviroment.ts ; guitar-data.service.ts ) [Closes #8]

--- src/app/guitar-data.service.ts
+++ src/app/guitar-data.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Guitar } from './guitar-list/guitar';
 
 
-const URL = "http://localhost:3000/guitars";
+
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,6 @@ export class GuitarDataService {
 
   public getAll(): Observable<Guitar[]> {
 
-    return this.http.get<Guitar[]>(URL);
+    return this.http.get<Guitar[]>(environment.apiUrl);
   }
 }
