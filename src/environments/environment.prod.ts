0d9d81aabeb39228e3e3e7589a8720f6abc65851
46u571N <leo.gardey@gmail.com>
2022-11-17 18:31:30 -0300

    Update enviroment to Render

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl:"https://tec-web-guitar-app.herokuapp.com/guitars"
+  //apiUrl:"https://tec-web-guitar-app.herokuapp.com/guitars"
+    apiUrl:"https://nest-ci-app.onrender.com"
  
 };
